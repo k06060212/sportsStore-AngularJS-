df680cb629f6cde1d297a6c0153f652660e870f4
k06060212 <k06060212@gmail.com>
2021-04-27 12:24:54 +0900

    챕터 1.. username 에 따른 주문 내역과 상세 정보들..

--- controllers/checkoutControllers.js
+++ controllers/checkoutControllers.js
@@ -4,16 +4,16 @@ angular.module("sportsStore")
     $scope.cartData = cart.getProducts();
 
     $scope.total = function(){
+        var total = 0;
 
-        $scope.total = function(){
-            for(var i = 0; i < $scope.cartData.length; i++){
-                total += ($scope.cartData[i].price * $scope.cartData[i].count);
-            }
-            return total;
+        for(var i = 0; i < $scope.cartData.length; i++){
+            total += ($scope.cartData[i].price * $scope.cartData[i].count);
         }
+        return total;
+    }
 
         $scope.remove = function(id){
             cart.removeProduct(id);
         }
-    }
+    
 });
